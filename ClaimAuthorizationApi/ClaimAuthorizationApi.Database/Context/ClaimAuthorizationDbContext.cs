7111140774da66f652e60d8f900599c3e32ed30e
Sunil <sunil_karmakar@ymail.com>
2022-01-20 20:58:23 +0600

    Create Project
    1. Create project ClaimAuthorizationApi, ClaimAuthorizationApi.Model,  ClaimAuthorizationApi.Database,

---
+++ ClaimAuthorizationApi/ClaimAuthorizationApi.Database/Context/ClaimAuthorizationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ClaimAuthorizationApi.Model.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClaimAuthorizationApi.Database.Context
+{
+    public class ClaimAuthorizationDbContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        public ClaimAuthorizationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ClaimAuthorizationApi/ClaimAuthorizationApi.Model/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ClaimAuthorizationApi.Model.Models
+{
+    public class User : IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime LastModifiedTime { get; set; }
+    }
+}

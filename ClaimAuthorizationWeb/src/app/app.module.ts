79e21662cbefb1ac1754cd2774c0f12ca57fa2be
Sunil <sunil_karmakar@ymail.com>
2022-03-08 15:08:01 +0600

    Create Angular Project
    1. Create Angular Project.
    2. Create Login Component.

---
+++ ClaimAuthorizationWeb/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      LoginComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
